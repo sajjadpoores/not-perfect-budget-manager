e9d86fb9a62ca72e2d7cec1c65e8afe315b4ee6b
Amir Sajjad Poursoltani <sajjadpoores@gmail.com>
2024-08-20 22:29:50 +0330

    feature(app.module): added configModule to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExpenseModule } from './expense/expense.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [ExpenseModule],
+  imports: [ExpenseModule, ConfigModule.forRoot({envFilePath:  '.env'})],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
