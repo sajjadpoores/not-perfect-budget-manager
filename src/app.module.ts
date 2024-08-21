d7ef579bd34cbfb40baf4980aada6785b9bb69ad
Amir Sajjad Poursoltani <sajjadpoores@gmail.com>
2024-08-22 00:42:31 +0330

    feature(user): added new module called user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ExpenseModule } from './modules/expense/expense.module';
+import { UserModule } from './modules/user/user.module';
 
 
 @Module({
-  imports: [ExpenseModule, ConfigModule.forRoot({envFilePath:  '.env'})],
+  imports: [ExpenseModule, ConfigModule.forRoot({envFilePath:  '.env'}), UserModule],
   controllers: [],
   providers: [],
 })
